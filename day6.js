1e1d7fa94c07af339ea70cbc08c5b55de2346c9d
Zachary Partal <zpartal@gmail.com>
2015-12-06 01:59:36 -0600

    Day 6 Part 2

--- day6.js
+++ day6.js
@@ -3,40 +3,41 @@
 var fs = require('fs');
 var input = fs.readFileSync('day6_input.txt').toString().split('\n');
 
-var gridWidth = 1000;
+var lights = { A : [], B : [] };
 
-var lights = [];
-for (var x = 0; x < gridWidth; ++x) {
-  lights.push([]);
-  for (var y = 0; y < gridWidth; ++y)
-    lights[x].push(false); 
+for(var i=0; i<1000; i++) { lights.A[i] = []; lights.B[i] = []; }
+
+function updateLightsA(lights,mode,x1,y1,x2,y2) {
+  for (var x = x1; x <= x2; ++x)
+    for (var y = y1; y <= y2; ++y)
+      lights[y][x] = (mode === "toggle") ? !lights[y][x] : (mode === "on");
+}
+
+function updateLightsB(lights,mode,x1,y1,x2,y2) {
+  for (var x = x1; x <= x2; ++x)
+    for (var y = y1; y <= y2; ++y) {
+      lights[y][x] = lights[y][x] || 0;
+      if (mode === "on" ) ++lights[y][x];
+      else if (mode === "off" && lights[y][x] > 0) --lights[y][x];
+      else if (mode === "toggle") lights[y][x] += 2;
+    }
 }
 
 input.forEach(function(val) {
-  var splitInp = val.split(' ');
-  var mode = "";
-  var trc,blc = [];
-  if (splitInp.indexOf("toggle") > -1) {
-    mode = splitInp[0];
-    trc  = splitInp[1].split(',');
-    blc  = splitInp[3].split(',');
-  }
-  else {
-    mode = splitInp[1];
-    trc  = splitInp[2].split(',');
-    blc  = splitInp[4].split(',');
-  }
-  
-  for (var x = Number(trc[0]); x <= Number(blc[0]); ++x)
-      for (var y = Number(trc[1]); y <=Number(blc[1]); ++y)
-        lights[y][x] = (mode === "toggle") ? !lights[y][x] : (mode === "on");
+  var regEx = /(on|off|toggle)\D+(\d+),(\d+)\D+(\d+),(\d+)/;
+  var cmd = regEx.exec(val);
 
+  updateLightsA(lights.A,cmd[1],Number(cmd[2]),Number(cmd[3]),Number(cmd[4]),Number(cmd[5]));
+  updateLightsB(lights.B,cmd[1],Number(cmd[2]),Number(cmd[3]),Number(cmd[4]),Number(cmd[5]));
 },lights);
 
-var output = 0;
-for (var x = 0; x < gridWidth; ++x)
-  for (var y = 0; y < gridWidth; ++y)
-    if (lights[x][y]) ++output;
+var output1 = 0;
+var output2 = 0;
+for (var x = 0; x < 1000; ++x)
+  for (var y = 0; y < 1000; ++y) {
+    output1 += lights.A[y][x] || 0;
+    output2 += lights.B[y][x] || 0;
+  }
 
-console.log(output);
-
+console.log(output1);
+console.log(output2);
